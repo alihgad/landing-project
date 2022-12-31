4a8e66ab2e99ccbc9ca46b9173d9263ebb91b923
alihgad <alihgad2@gmail.com>
2023-01-01 01:35:45 +0200

    prety

--- js/app.js
+++ js/app.js
@@ -24,9 +24,9 @@
 */
 
 // select sections from html by element
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 // select the list from html by element
-const ul = document.querySelector(".navbar__menu");
+const ul = document.querySelector('.navbar__menu');
 
 /**
  * End Global Variables
@@ -43,35 +43,35 @@ function craeteList() {
     //make loop to loop on evrey ection
     sections.forEach(function (section) {
         //create li + a
-        const li = document.createElement("li");
-        let a = document.createElement("a");
+        const li = document.createElement('li');
+        let a = document.createElement('a');
         // add text and href and class to change style  
         a.innerText = `section ${section.innerText[8]}`
         // link a with sections
-        a.setAttribute("href", `#section${section.innerText[8]}`)
+        a.setAttribute('href', `#section${section.innerText[8]}`)
         //give it class to style it
-        a.setAttribute("class", "menu__link");
+        a.setAttribute('class', 'menu__link');
         //append it to freg dom
         li.appendChild(a);
         freg.appendChild(li);
-        li.setAttribute("class","link");
-        li.setAttribute("style", "font-size:14px;");
+        li.setAttribute('class','link');
+        li.setAttribute('style', 'font-size:14px;');
         for (let i=0;i<sections.length;i++){
-            li.setAttribute("index",`${i}`)
+            li.setAttribute('index',`${i}`)
         }
     })
     //append freg dom to list 
     ul.appendChild(freg)
-    ul.setAttribute("style","display:flex;")
+    ul.setAttribute('style','display:flex;')
 };
 // make function to add class activ to section when be in view port
 function activ() {
     for (let i = 0; i < sections.length; i++) {
         // add the class when section be in view port
         if (sections[i].getBoundingClientRect().top < 300) {
-            document.getElementById(`section${i + 1}`).classList.add("your-active-class");
+            document.getElementById(`section${i + 1}`).classList.add('your-active-class');
         } else {
-            document.getElementById(`section${i + 1}`).classList.remove("your-active-class");
+            document.getElementById(`section${i + 1}`).classList.remove('your-active-class');
         }
     }
 };
@@ -88,7 +88,7 @@ craeteList();
  *
 */
 // make the event to add active class while scrolling
-window.addEventListener("scroll",activ);
+window.addEventListener('scroll',activ);
 // Build menu
 
 // Scroll to section on link click
@@ -98,10 +98,10 @@ const li = document.querySelectorAll('.link');
 
 // use loop to add event smooth scrool
 for (let i =0;i<sections.length;i++){
-    li[i].addEventListener("click", evt => {
+    li[i].addEventListener('click', evt => {
         evt.preventDefault()
         sections[i].scrollIntoView({
-            behavior : "smooth"
+            behavior : 'smooth'
         })
     });
 }
